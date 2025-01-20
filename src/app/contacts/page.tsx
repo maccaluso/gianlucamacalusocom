c74c1e896eca70d4813bac284396d3194c5dd14f
maccaluso <maccaluso@gmail.com>
2025-01-20 11:09:13 +0100

    contact

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,5 +1,17 @@
+import Link from "next/link"
+
 export default function Contacts () {
   return (
-    <div className="pageContainer">CONTACTS</div>
+    <div className={'pageContainer contactPage'}>
+      <ul>
+        <li>
+          <Link href="https://www.linkedin.com/in/maccaluso/">Linkedin</Link>
+        </li>
+
+        <li>
+          <Link href="mailto:maccaluso@gmail.com">Email</Link>
+        </li>
+      </ul>
+    </div>
   )
 }
